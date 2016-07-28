29637fa12e39979fc65f427b7252576aa8946c94
spyshiv <spyshiv@gmail.com>
2016-07-29 02:39:00 +0530

    Add pie chart

--- public/static/js/controller.js
+++ public/static/js/controller.js
@@ -53,6 +53,24 @@ app.controller('TrendCtrl', ['$scope', '$http', function ($scope, $http) {
 		if($scope.firstctrends && $scope.secondctrends) {
 			$scope.commontrends = $scope.getIntersection($scope.firstctrends, $scope.secondctrends);
 		}
+
+		var commontrends = $scope.commontrends;
+
+		var totaltrendwidth = 0;
+		angular.forEach(commontrends, function(object, index){
+			totaltrendwidth += object.name.length;
+		});
+
+		$scope.piedata = [];
+		angular.forEach(commontrends, function(object, index){
+			var obj = {};
+			obj.trendname = object.name;
+			obj.trendlength = (object.name.length/totaltrendwidth)*100;
+			$scope.piedata.push(obj);
+		});
+
+		$scope.piechart($scope.piedata);
+
     }
 
     $scope.getIntersection = function(firstctrends, secondctrends) {
@@ -66,4 +84,55 @@ app.controller('TrendCtrl', ['$scope', '$http', function ($scope, $http) {
 		})
 		return commontrends;
 	}
+
+	$scope.piechart = function(data){
+		
+		d3.select("svg").remove();
+		$scope.colors = [];
+		var width = 450,
+		    height = 300,
+		    radius = Math.min(width, height) / 2;
+
+		var color = d3.scale.category10();
+
+		var arc = d3.svg.arc()
+		    .outerRadius(radius - 100)
+		    .innerRadius(radius - 10);
+
+		var pie = d3.layout.pie()
+		    .sort(null)
+		    .value(function(d) { return d.trendlength; });
+
+		var svg = d3.select("#pie-chart").append("svg")
+		    .attr("width", width)
+		    .attr("height", height)
+		  	.append("g")
+		    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+		var g = svg.selectAll(".arc")
+		    .data(pie(data))
+		    .enter().append("g")
+		    .attr("class", "arc");
+
+	  	g.append("path")
+	      	.attr("d", arc)
+	      	.style("fill", function(d) { 
+	      		$scope.colors.push(color(d.data.trendname));
+	      		return color(d.data.trendname); });
+
+	  	g.append("text")
+	      	.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+	      	.attr("dy", ".35em")
+	      	.text(function(d) { return d.data.trendlength; });
+
+		function type(d) {
+		  	d.trendlength = +d.trendlength;
+		  	return d;
+		}
+
+
+	}
+
+	
+
 }]);
